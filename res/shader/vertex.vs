314e1232fa27857ee6b1f2aa873da9aea7875a05
crittericallyonline <ballstenny@gmail.com>
2025-12-14 13:59:38 -0800

    FUCK YEAH

--- res/shader/vertex.vs
+++ res/shader/vertex.vs
@@ -11,7 +11,7 @@ varying vec2 TexCoord;
 
 void main()
 {
-    gl_Position = projection_matrix * modelview_matrix * vec4(vertex_position, 1.0);
-    // gl_Position = vec4(vertex_position, 1.0);
+    // gl_Position = projection_matrix * modelview_matrix * vec4(vertex_position, 1.0);
+    gl_Position = vec4(vertex_position, 1.0);
     TexCoord = vec2(texCoord.x, 1.0-texCoord.y);
 }
--- src/objects.h
+++ src/objects.h
@@ -29,14 +29,15 @@ Object *create_triangle()
     glGenVertexArrays(1, &array);
     glBindVertexArray(array);
 
+    // vertex
     glGenBuffers(1, &verticiesBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, verticiesBuffer);
-    glBufferData(GL_ARRAY_BUFFER, 3, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // indicies
     glGenBuffers(1, &indicesBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer);
-
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) , indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(Program.vertex_position, 3, GL_FLOAT, false, 0, NULL);
     glEnableVertexAttribArray(Program.vertex_position);
@@ -54,7 +55,9 @@ Object *create_triangle()
 
 void drawModel(Object *model)
 {
+    glEnableVertexAttribArray(Program.vertex_position);
     glBindVertexArray(model->VAO);
-    glDrawArrays(GL_LINES, 0, model->vert_count);
+    glDrawArrays(GL_TRIANGLES, 0, model->vert_count);
     glBindVertexArray(0);
+    glDisableVertexAttribArray(Program.vertex_position);
 }
